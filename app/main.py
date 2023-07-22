2bbe0fff456a36670cd4e7b802279a0f17ea1a12
nk6june <nkjune6@gmail.com>
2023-07-22 18:02:00 +0530

    First Commit

---
+++ app/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from langcorn import create_service
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from app.conversation import conversation
+
+
+class Input(BaseModel):
+    human_input: str
+
+class Output(BaseModel):
+    output: str
+
+app = FastAPI()
+
+@app.post("/conversation")
+async def input(input: Input):
+    output = Output(output=conversation(input.human_input))
+    return output
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
