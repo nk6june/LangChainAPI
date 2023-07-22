885a338f73c41125d6c01158e3c251596bb270a5
nk6june <nkjune6@gmail.com>
2023-07-23 00:48:12 +0530

    Added doc strings to main file

--- app/main.py
+++ app/main.py
@@ -6,15 +6,30 @@ from app.conversation import conversation
 
 
 class Input(BaseModel):
+    """
+    Input model for the conversation endpoint.
+    """
     human_input: str
 
 class Output(BaseModel):
+    """
+    Output model for the conversation endpoint.
+    """
     output: str
 
 app = FastAPI()
 
 @app.post("/conversation")
 async def input(input: Input):
+    """
+    Endpoint to carry out a conversation with the LangCorn service.
+
+    Parameters:
+        - input (Input): Input model containing the user's message.
+
+    Returns:
+        Output: Output model containing LangCorn's response.
+    """
     output = Output(output=conversation(input.human_input))
     return output
 
