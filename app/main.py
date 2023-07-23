3a993a13418e891357d989f6bdaedff2ff91693a
nk6june <nkjune6@gmail.com>
2023-07-23 22:35:47 +0530

    Modified main file

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from langcorn import create_service
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from app.conversation import conversation
+from conversation import conversation
 
 
 class Input(BaseModel):
